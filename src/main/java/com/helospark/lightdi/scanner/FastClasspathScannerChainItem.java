17f8dd49152f2086f89b0519360b7de7e6ff6892
helospark <helospark@gmail.com>
2017-09-30 09:40:10 +0200

    Made FastClasspathScanner optional dependency

--- src/main/java/com/helospark/lightdi/scanner/FastClasspathScannerChainItem.java
+++ src/main/java/com/helospark/lightdi/scanner/FastClasspathScannerChainItem.java
@@ -12,9 +12,12 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 public class FastClasspathScannerChainItem implements ClasspathScannerChainItem {
     private static final Logger LOGGER = LoggerFactory.getLogger(FastClasspathScannerChainItem.class);
 
+    private boolean isFastClasspathDependencyAlreadyChecked = false;
+
     @Override
     public List<String> scan(ComponentScanPackage componentScanPackage) {
         LOGGER.debug("Scanning " + componentScanPackage + " with fastclasspathscanner");
+        assertFastClasspathScannerDependencyOnClasspath();
         return new FastClasspathScanner(componentScanPackage.getPackageName())
                 .scan()
                 .getNamesOfAllClasses();
@@ -22,7 +25,26 @@ public class FastClasspathScannerChainItem implements ClasspathScannerChainItem 
 
     @Override
     public boolean doesSupport(ComponentScanPackage componentScanPackage) {
-        return true; // always supports
+        return true; // always supported
+    }
+
+    private void assertFastClasspathScannerDependencyOnClasspath() {
+        if (!isFastClasspathDependencyAlreadyChecked) {
+            if (!isFastClassPathScannerOnClasspath()) {
+                throw new IllegalStateException(
+                        "Full classpath scan requested, but FastClasspathScanner is not added to classpath. Please add it as a dependency.");
+            }
+            isFastClasspathDependencyAlreadyChecked = true;
+        }
+    }
+
+    private boolean isFastClassPathScannerOnClasspath() {
+        try {
+            Class.forName("io.github.lukehutch.fastclasspathscanner.FastClasspathScanner");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
     }
 
 }
